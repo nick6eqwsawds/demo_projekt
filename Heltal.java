fefa0fe39443e1602eef04c88c44d24d33171f5a
otto hoffman <otto.hoffman@outlook.com>
2023-09-06 10:16:59 +0200

    laggt till kod för heltals operationer

---
+++ Heltal.java
@@ -0,0 +1,19 @@
+public class Heltal {
+    public static void main(String[] args) throws Exception{
+        int tal1 = 2;
+        int tal2 = 30;
+
+        int summa_addition = tal1+tal2;
+        int summa_subtraktion = tal1-tal2;
+
+        System.out.println("summan av tal + tal2 är "+summa_addition);
+        System.out.println("summan av tal - tal2 är "+summa_subtraktion);
+
+        int produkt = tal1*tal2;
+        int kvot = tal1/tal2;
+
+        System.out.println("Produkten av tal1*tal2 är "+produkt);
+        System.out.println("Kvoten av tal1/tal2 är "+kvot);
+    }
+    
+}
