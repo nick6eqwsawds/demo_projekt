c072a7d616c703877db3506e7fa5d29846fd20c8
otto hoffman <otto.hoffman@outlook.com>
2023-09-06 10:49:01 +0200

    uppgift A addition

---
+++ uppg3.java
@@ -0,0 +1,14 @@
+public class uppg3 {
+    public static void main(String[] args){
+        System.out.println("Uppg A");
+        int tal1 = 23;
+        int tal2 = 45;
+        int summa;
+        int summa_subtraktion;
+        int produkt;
+        summa = tal1+tal2;
+        summa_subtraktion = tal1-tal2;
+        System.out.println(tal1+"+"+tal2+"="+summa);
+        System.out.println(tal1+"-"+tal2+"="+summa_subtraktion);
+    }
+}
