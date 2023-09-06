5ae8356e5124fec3c22e98298ff9a2bae3e0e4a9
otto hoffman <otto.hoffman@outlook.com>
2023-09-06 10:54:49 +0200

    beräkning produkt stora tal uppgift B

--- uppg3.java
+++ uppg3.java
@@ -3,12 +3,19 @@ public class uppg3 {
         System.out.println("Uppg A");
         int tal1 = 23;
         int tal2 = 45;
+        int tal3 = 234232;
+        int tal4 = 3424;
         int summa;
         int summa_subtraktion;
         int produkt;
+        produkt = tal3*tal4;
         summa = tal1+tal2;
         summa_subtraktion = tal1-tal2;
         System.out.println(tal1+"+"+tal2+"="+summa);
         System.out.println(tal1+"-"+tal2+"="+summa_subtraktion);
+
+        System.out.println("Uppg B");
+        System.out.println(tal3+"*"+tal4+"="+produkt);
+        System.out.println(tal2+"*"+tal3+"*"+tal4+"="+produkt*tal2);
     }
 }
