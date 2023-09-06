cafebc36f9d628600495d22a905bb2ddd95293d9
otto hoffman <otto.hoffman@outlook.com>
2023-09-06 10:20:44 +0200

    laggt till modulo kod

--- Heltal.java
+++ Heltal.java
@@ -14,6 +14,12 @@ public class Heltal {
 
         System.out.println("Produkten av tal1*tal2 är "+produkt);
         System.out.println("Kvoten av tal1/tal2 är "+kvot);
+
+        int rest1 = 10%3;
+        int rest2 = 9%3;
+
+        System.out.println("Resten av 10/3 är "+rest1);
+        System.out.println("Resten av 9/3 är "+rest2);
     }
     
 }
