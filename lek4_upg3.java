0d410582ab6460e1a0f9a652552609cf18b060fe
otto hoffman <otto.hoffman@outlook.com>
2023-09-13 10:57:47 +0200

    lek4_upg3

---
+++ lek4_upg3.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class lek4_upg3 {
+    public static void main(String[] args) throws Exception {
+    System.out.println("skriv ditt förnamn, efternamn, läng, ålder och vikt med mellanslag mellan sig:");
+    }
+}
