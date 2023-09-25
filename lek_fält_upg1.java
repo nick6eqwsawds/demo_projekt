7b8a0b7697f075825eb4c655caa19d8053da8ce0
otto hoffman <otto.hoffman@outlook.com>
2023-09-25 09:34:18 +0200

    lek fält upg1 C delen tangentbord till array

--- lek_fält_upg1.java
+++ lek_fält_upg1.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class lek_fält_upg1 {
     public static void main(String[] args) {
+                try (Scanner tangentbord = new Scanner(System.in)) {
         int[] fält1 = {3, 4, 6, 8, 9};
         for (int i = 0; i < fält1.length; i++) {
             System.out.println(fält1[i]);
@@ -21,6 +24,18 @@ public class lek_fält_upg1 {
 
         int[] summafält = {fält1[0]+fält2[0], fält1[1]+fält2[1], fält1[2]+fält2[2], fält1[3]+fält2[3], fält1[4]+fält2[4] };
         System.out.println("summafält variablar är: "+summafält[0]+" "+summafält[1]+" "+summafält[2]+" "+summafält[3]+" "+summafält[4]);
+        
+        System.out.println("Skriv in 4 tal");
+        int[] tangentbordsfält = new int[4];
+        tangentbordsfält[0] = tangentbord.nextInt();
+        tangentbordsfält[1] = tangentbord.nextInt();
+        tangentbordsfält[2] = tangentbord.nextInt();
+        tangentbordsfält[3] = tangentbord.nextInt();
+
+        int tangentbordfält_summa = tangentbordsfält[0]+tangentbordsfält[1]+tangentbordsfält[2]+tangentbordsfält[3];
+        System.out.println("Summan av dina tal är: "+tangentbordfält_summa);
+
+        }
     }
 }
 
